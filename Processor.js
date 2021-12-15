3087527e42fb61b44a92812b44570971e07862ce
kleitonalbuquerque <kleiton2102@gmail.com>
2021-12-15 11:45:08 -0300

    Processador de dados

---
+++ Processor.js
@@ -0,0 +1,15 @@
+class Processor {
+  static Process(data) {
+    var datasCSV = data.split("\r\n");
+    var rows = [];
+
+    datasCSV.forEach((row) => {
+      var arr = row.split(",");
+      rows.push(arr);
+    });
+    return rows;
+    // console.log(rows);
+  }
+}
+
+module.exports = Processor;
--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var Reader = require("./Reader");
+var Processor = require("./Processor");
 
 var read = new Reader();
 
 async function main() {
-  var data = await read.Read("./users.csv");
-  // var dataConvertToJSON = JSON.parse(data);
+  var dados = await read.Read("./users.csv");
 
-  console.log(data);
+  var dadosProcessados = Processor.Process(dados);
 }
 
 main();
